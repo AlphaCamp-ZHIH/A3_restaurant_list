ad1b608c1c4400a5973fe658e97875854872ac06
zhih <fufong79570@gmail.com>
2021-04-20 09:42:08 +0800

    Delete unnessnary infomation

--- app.js
+++ app.js
@@ -15,16 +15,18 @@ app.set('view engine', 'hbs');
 
 app.get('/restaurants/:id', (req, res) => {
   const id = req.params.id;
-  // console.log(id)
   const restaurant = restaurants.find(item => item.id === +id);
   res.render('show', { pageTitle: restaurant.name, restaurant: restaurant })
 });
 
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword;
-  // console.log(keyword)
-  filterList = restaurants.filter(restaurant => restaurant.name.includes(keyword) || restaurant.name_en.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(keyword.toLowerCase()));
-  // console.log(filterList)
+
+  const filterList = restaurants.filter(restaurant =>
+    restaurant.name.includes(keyword)
+    || restaurant.name_en.toLowerCase().includes(keyword.toLowerCase())
+    || restaurant.category.toLowerCase().includes(keyword.toLowerCase()));
+
   res.render('index', { pageTitle: 'index', isIndex: true, restaurants: filterList })
 
 });
